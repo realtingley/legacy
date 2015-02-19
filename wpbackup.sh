8b743400ec38a3babc8d85c0d2a06841fa502313
Scott <realtingley@users.noreply.github.com>
2015-02-19 17:33:21 -0500

    Quotes on date variables
    Prevent word splitting

--- wpbackup.sh
+++ wpbackup.sh
@@ -5,15 +5,15 @@ USER="USERNAME"
 PASS="PASSWORD"
 
 # Backup MySQL databases for WordPress
-mysqldump -u$USER -p$PASS --all-databases > /var/www/SITE/alldatabases.$(date +%F).sql | gzip > /var/www/SITE/alldatabases.$(date +%F).sql.gz
-rm /var/www/SITE/alldatabases.$(date +%F).sql
+mysqldump -u$USER -p$PASS --all-databases > /var/www/SITE/alldatabases."$(date +%F)".sql | gzip > /var/www/SITE/alldatabases."$(date +%F)".sql.gz
+rm /var/www/SITE/alldatabases."$(date +%F)".sql
 
 # Remove MySQL backups that are over a month old from the /var/www/SITE/ directory
 find /var/www/SITE/alldatabases.*.sql.gz -mtime +21 -exec rm {} \;
 
 # Copy the directory to the EBS
-mkdir -p /webdata/WPbackup/wordpress.$(date +%F).bak
-cp -r /var/www/SITE/* /webdata/WPbackup/wordpress.$(date +%F).bak/
+mkdir -p /webdata/WPbackup/wordpress."$(date +%F)".bak
+cp -r /var/www/SITE/* /webdata/WPbackup/wordpress."$(date +%F)".bak/
 
 # Remove backups that are over a month old
 find /webdata/WPbackup/* -mtime +21 -exec rm -r {} \;
